f486ec16a684307f80051c403afbe87792a75291
temni <temni@temni-P5K-SE.(none)>
2012-03-29 00:27:45 +1100

    Now sceleton is ready. @TODO: recursive main function =)

--- script.php
+++ script.php
@@ -1,4 +1,20 @@
 <?php
 require 'config.php';
-echo $host_;
-?>
+/* $host_; $user; $key*/
+require 'includes/cloudfiles.php';
+
+	$auth = new CF_Authentication($user, $key, '', $host_);
+	try 
+	{
+		$auth->authenticate();
+	}
+	catch(Exception $e)
+	{
+		die('Auth error! Check username and password in your config file');
+	}
+
+	$conn = new CF_Connection($auth);
+	if (!$conn) die('Something goes wrong');
+
+	$container_list = $conn->list_containers();
+	print_r($container_list);
